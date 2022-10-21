ca25adb75447979761b1256d5210336fba484e70
Gertobin11 <gertobin11@gmail.com>
2022-10-21 16:40:27 +0100

    Add footer and add Leaflet css and js

--- home/static/home/js/home.js
+++ home/static/home/js/home.js
@@ -1,15 +1,33 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-          const intersecting = entry.isIntersecting
-          if (intersecting){
-            entry.target.classList.add('fade-up')
-            entry.target.classList.remove('faded')
-          }
-        })
-      },{ rootMargin: "-40px" })
-      const cards = Array.from(document.getElementsByClassName('package-card'))
-      cards.forEach((card) => {
-        observer.observe(card)
-      })
-})
+window.addEventListener("DOMContentLoaded", () => {
+  /*
+   * Add animations when cards enter the view
+   */
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        const intersecting = entry.isIntersecting;
+        if (intersecting) {
+          entry.target.classList.add("fade-up");
+          entry.target.classList.remove("faded");
+        }
+      });
+    },
+    { rootMargin: "-40px" }
+  );
+  const cards = Array.from(document.getElementsByClassName("package-card"));
+  cards.forEach((card) => {
+    observer.observe(card);
+  });
+  /*
+   * Add JavaScript for Leaflet maps
+   */
+  var map = L.map("map").setView([51.505, -0.09], 13);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+  map.setView(new L.LatLng(52.299, -9.657), 10);
+  var marker = L.marker([52.299, -9.657]).addTo(map);
+  marker.bindPopup("<b>Kerry Paintball</b><br>Located just outside Tralee on the Listowel road.").openPopup();
+});
--- kerry_paintball/settings.py
+++ kerry_paintball/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-if os.path.exists("env.py"):
+if os.path.isfile("env.py"):
     import env
 
 # SECURITY WARNING: keep the secret key used in production secret!
