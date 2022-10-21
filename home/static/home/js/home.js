0be0c54e3dfca3dbc91693f899ca03e35d8a0cf6
Gertobin11 <gertobin11@gmail.com>
2022-10-21 16:46:21 +0100

    Create base.js and map JS inside

--- home/static/home/js/home.js
+++ home/static/home/js/home.js
@@ -18,16 +18,4 @@ window.addEventListener("DOMContentLoaded", () => {
   cards.forEach((card) => {
     observer.observe(card);
   });
-  /*
-   * Add JavaScript for Leaflet maps
-   */
-  var map = L.map("map").setView([51.505, -0.09], 13);
-  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-    attribution:
-      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  }).addTo(map);
-  map.setView(new L.LatLng(52.299, -9.657), 10);
-  var marker = L.marker([52.299, -9.657]).addTo(map);
-  marker.bindPopup("<b>Kerry Paintball</b><br>Located just outside Tralee on the Listowel road.").openPopup();
 });
---
+++ static/js/base.js
@@ -0,0 +1,14 @@
+window,addEventListener('DOMContentLoaded', () => {
+    /*
+   * Add JavaScript for Leaflet maps
+   */
+  var map = L.map("map").setView([51.505, -0.09], 13);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+  map.setView(new L.LatLng(52.299, -9.657), 10);
+  var marker = L.marker([52.299, -9.657]).addTo(map);
+  marker.bindPopup("<b>Kerry Paintball</b><br>Located just outside Tralee on the Listowel road.").openPopup();
+})
