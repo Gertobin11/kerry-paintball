1e437c213b49deb57ca2bf1b86cfd551e1a5cfe4
Gertobin11 <gertobin11@gmail.com>
2022-10-24 16:06:58 +0100

    Fix typos and add link to gallery in index.html

--- static/js/base.js
+++ static/js/base.js
@@ -18,7 +18,6 @@ window,addEventListener('DOMContentLoaded', () => {
   const burgerToggler = document.getElementsByClassName('navbar-toggler')[0];
   let isOpen = false;
   burgerToggler.addEventListener('click', () => {
-    console.log(isOpen)
     if (!isOpen) {
       burgerToggler.classList.add('open');
       isOpen = true;
