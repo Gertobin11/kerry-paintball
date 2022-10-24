a3ed1dee082576c9468e7b3df8ff6727ca91c075
Gertobin11 <gertobin11@gmail.com>
2022-10-24 08:18:15 +0100

    Add crispy forms, custom css and custom js to style the contact form page
    Add css rules to make the site more responsive. Fix an issue where navbar would expand on page load

--- contact/views.py
+++ contact/views.py
@@ -7,9 +7,9 @@ class Contact(SuccessMessageMixin, CreateView):
     form_class = ContactForm
     template_name = 'contact_form.html'
     success_url = '/'
-    success_message = 'Your enquiry has been successfully recieved, we will be in contact shortly'
+    success_message = ('Your enquiry has been successfully '
+                       'recieved,we will be in contact shortly')
 
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
-
--- staticfiles/js/base.js
+++ staticfiles/js/base.js
@@ -11,4 +11,21 @@ window,addEventListener('DOMContentLoaded', () => {
   map.setView(new L.LatLng(52.299, -9.657), 10);
   var marker = L.marker([52.299, -9.657]).addTo(map);
   marker.bindPopup("<b>Kerry Paintball</b><br>Located just outside Tralee on the Listowel road.").openPopup();
+
+  /*
+   * Add Animation to the burger Menu 
+  */
+  const burgerToggler = document.getElementsByClassName('navbar-toggler')[0];
+  let isOpen = false;
+  burgerToggler.addEventListener('click', () => {
+    console.log(isOpen)
+    if (!isOpen) {
+      burgerToggler.classList.add('open');
+      isOpen = true;
+    }
+    else {
+      burgerToggler.classList.remove('open');
+      isOpen = false;
+    }
+  })
 })
