09dfa575967df26f4ad2476b3100e3c8035e6ece
Gertobin11 <gertobin11@gmail.com>
2022-10-24 13:07:46 +0100

    Add grid layout with images, Add custom css fade in

--- gallery/static/js/gallery.js
+++ gallery/static/js/gallery.js
@@ -14,4 +14,16 @@ window.addEventListener("DOMContentLoaded", () => {
     const header = document.getElementsByClassName("gallery-header")[0];
     console.log(header)
     observer.observe(header);
+    /*
+     * Apply a fade in effect on the images loading in
+     */
+    images = Array.from(document.getElementsByClassName('gallery-item'))
+    wait = 0
+    images.forEach((image) => {
+        wait += 200
+        setTimeout(() => {
+            image.classList.add('fade-up')
+            image.classList.remove('faded')
+        }, 200 + wait)
+    })
   });
