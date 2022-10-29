1fe64061cbfcd6638fe277b488da60544b25a0ec
Gertobin11 <gertobin11@gmail.com>
2022-10-29 13:36:50 +0000

    Add CNAME host to alloed_hosts

--- kerry_paintball/settings.py
+++ kerry_paintball/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.paintballkerry.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.paintballkerry.onrender.com',
+                 '.paintballkerry.com', '.kerrypaintball.com']
 X_FRAME_OPTIONS = 'SAMEORIGION'
 
 
