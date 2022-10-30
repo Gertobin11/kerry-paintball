11f1a71e670c347b6dd145ebfb74cd94786f355f
Gertobin11 <gertobin11@gmail.com>
2022-10-30 11:51:56 +0000

    Fix typo in settings.py

--- kerry_paintball/settings.py
+++ kerry_paintball/settings.py
@@ -161,16 +161,17 @@ else:
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.environ.get('EMAIL SENDER')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_SENDER')
+ADMIN_EMAIL =os.environ.get('ADMIN_EMAIL')
 
 
 # Default primary key field type
