952a8d1e0bcb069332920327997d8205f225c5b4
Gertobin11 <gertobin11@gmail.com>
2023-04-10 11:17:14 +0100

    Moved allowed hst to the env file

--- kerry_paintball/settings.py
+++ kerry_paintball/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'PAINTBALL_DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.paintballkerry.onrender.com',
-                 '.paintballkerry.com', '.kerrypaintball.com']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
 X_FRAME_OPTIONS = 'SAMEORIGION'
 
 SITE_ID = 1
